02efb36f7eb94f4c16f6770fb8b553ad22c727c3
Brandon Morrell <brandon@morrellworld.net>
2019-10-22 14:51:22 -0500

    Added input boxes that move the ball

--- KinematicsHelp/Ball.java
+++ KinematicsHelp/Ball.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import javax.swing.*;
 /**
  * Write a description of class Ball here.
  * 
@@ -12,8 +12,22 @@ public class Ball extends Actor
      * Act - do whatever the Ball wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private boolean xChecked = false;
+    private boolean yChecked = false;
+    private String inputXValue;
+    private String inputYValue;
     public void act() 
     {
-        // Add your action code here.
+        if ( !xChecked )
+        {
+            inputXValue = JOptionPane.showInputDialog("Please input an x value");
+            xChecked = true;
+        }
+        if ( !yChecked )
+        {
+            inputYValue = JOptionPane.showInputDialog("Please input an y value");
+            yChecked = true;
+        }
+        setLocation(Integer.parseInt(inputXValue), Integer.parseInt(inputYValue));
     }    
 }
